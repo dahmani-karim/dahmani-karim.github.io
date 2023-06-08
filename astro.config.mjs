154648610dfdce02d1172339547235e27c36aa63
KARIM DAHMANI <karim.dahmani88@gmail.com>
2023-06-08 13:04:30 +0200

    MAJconfigBase

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,4 +3,5 @@ import { defineConfig } from 'astro/config';
 // https://astro.build/config
 export default defineConfig({
     site: 'https://dahmani-karim.github.io',
+    base: '/MyPortfolio',
 });
